cd46d5ec486019d4d7d5648c56a1e014b769d231
gustavorods <gustavo.rlsilva07@gmail.com>
2026-07-27 18:58:11 -0300

    test: add hello controller test

--- src/test/java/com/cicdlab/api/controller/HelloControllerTest.java
+++ src/test/java/com/cicdlab/api/controller/HelloControllerTest.java
@@ -11,6 +11,6 @@ public class HelloControllerTest {
 
         String response = controller.hello();
 
-        assertEquals("Hello CI/CDDDDDDD", response);
+        assertEquals("Hello CI/CD", response);
     }
 }
